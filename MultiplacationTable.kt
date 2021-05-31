a1a900e17a57512bfdf6fbb1dd6948f86a99ab8a
zumatrahia <84982254+zumatrahia@users.noreply.github.com>
2021-05-31 16:44:42 +0300

    Add files via upload

---
+++ MultiplacationTable.kt
@@ -0,0 +1,16 @@
+/* WAP to print multiplication table of an entered number */
+
+fun main() {
+    println("Enter Number")
+    val number = readLine().toString().toFloat()
+    println("$number * 1 = " + number * 1)
+    println("$number * 2 = " + number * 2)
+    println("$number * 3 = " + number * 3)
+    println("$number * 4 = " + number * 4)
+    println("$number * 5 = " + number * 5)
+    println("$number * 6 = " + number * 6)
+    println("$number * 7 = " + number * 7)
+    println("$number * 8 = " + number * 8)
+    println("$number * 9 = " + number * 9)
+    println("$number * 10 = " + number * 10)
+}
